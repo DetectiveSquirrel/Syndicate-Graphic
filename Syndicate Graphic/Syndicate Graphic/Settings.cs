37293a97245c1cd75af341ec2935c96fe4d81faa
Detective Squirrel <ratman7539@windowslive.com>
2018-12-27 00:35:56 +1300

    src

---
+++ Syndicate Graphic/Syndicate Graphic/Settings.cs
@@ -0,0 +1,21 @@
+﻿using PoeHUD.Hud.Settings;
+using PoeHUD.Plugins;
+using SharpDX;
+
+namespace Syndicate_Graphic
+{
+    public class Settings : SettingsBase
+    {
+        [Menu("X")]
+        public RangeNode<int> XPos { get; set; } = new RangeNode<int>(41, 0, 2000);
+        [Menu("Y")]
+        public RangeNode<int> YPos { get; set; } = new RangeNode<int>(136, 0, 2000);
+        [Menu("Width")]
+        public RangeNode<int> Width { get; set; } = new RangeNode<int>(1827, 0, 2000);
+        [Menu("Height")]
+        public RangeNode<int> Height { get; set; } = new RangeNode<int>(742, 0, 2000);
+
+        [Menu("Transparency")]
+        public RangeNode<int> Transparency { get; set; } = new RangeNode<int>(100, 0, 255);
+    }
+}
---
+++ Syndicate Graphic/Syndicate Graphic/SyndicateGraphic.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PoeHUD.Plugins;
+using SharpDX;
+
+namespace Syndicate_Graphic
+{
+    public class SyndicateGraphic : BaseSettingsPlugin<Settings>
+    {
+        public SyndicateGraphic()
+        {
+            PluginName = "Syndicate Graphic";
+        }
+
+        public override void Render()
+        {
+            var window = GameController.Window.GetWindowRectangleReal().TopLeft;
+            var CursorPos = Mouse.GetCursorPositionVector() - window;
+            var CusorBox = new RectangleF(CursorPos.X, CursorPos.Y, 30, 30);
+            var TopLeftRegion = new RectangleF(0, 0, 80, 80);
+
+#if DEBUG
+#endif
+
+            if (!GameController.Game.IngameState.UIRoot.Children[1].Children[61].IsVisible)
+                return;
+
+            Graphics.DrawBox(TopLeftRegion, new Color(50, 50, 50, 100));
+
+            if (CusorBox.Intersects(TopLeftRegion))
+            {
+                //LogMessage("LOGGING", 1);
+                Graphics.DrawPluginImage(PluginDirectory + "\\InfoImage.png", new RectangleF(Settings.XPos, Settings.YPos, Settings.Width, Settings.Height), new Color(255, 255, 255, Settings.Transparency));
+            }
+        }
+    }
+}
